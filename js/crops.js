6a381e4ea49e43e3b2348541827b7538c056d51e
ayomahansani <ayohansi17@gmail.com>
2024-11-18 21:08:08 +0530

    finish crop save function

--- js/crops.js
+++ js/crops.js
@@ -40,9 +40,41 @@ $(document).ready(function () {
 });
 
 
-$('#newCropModal').click(function () {
+$('#newCropBtn').click(function () {
     loadFieldNamesComboBoxAndSetFieldCodes()
 });
+
+
+
+// -------------------------- The start - Function to fetch fields and populate the select element --------------------------
+function loadFieldNamesComboBoxAndSetFieldCodes() {
+    $.ajax({
+        url: "http://localhost:5052/cropMonitoringSystem/api/v1/fields",
+        type: "GET",
+        headers: {
+            "Authorization": "Bearer " + localStorage.getItem("token")
+        },
+        success: function (response) {
+
+            // Assuming response is an array of FieldDto objects
+            const selectedFieldName = $("#fieldNamesComboBox");
+            selectedFieldName.empty(); // Clear existing options
+            selectedFieldName.append('<option value="">Choose a field</option>'); // Add default option
+
+            // Populate the select element with field names and IDs
+            response.forEach(field => {
+                const option = `<option value="${field.fieldCode}">${field.fieldName}</option>`;
+                selectedFieldName.append(option);
+            });
+        },
+        error: function (error) {
+            console.error("Error fetching fields:", error);
+            showErrorAlert("Failed to load fields. Please try again later.");
+        }
+    });
+}
+// -------------------------- The end - Function to fetch fields and populate the select element --------------------------
+
 
 
 
@@ -126,6 +158,10 @@ $("#crop-save").on('click', () => {
             // clean the inputs values
             $("#newCropModal form").trigger('reset');
 
+            // Remove the image preview
+            $("#previewImage").attr("src", "#").hide(); // Reset the image source and hide it
+            $("#noImageText").show();                  // Show the "No image selected" text
+
         },
 
         error: function (error) {
@@ -144,34 +180,105 @@ $("#crop-save").on('click', () => {
 
 
 
-// -------------------------- The start - Function to fetch fields and populate the select element --------------------------
-function loadFieldNamesComboBoxAndSetFieldCodes() {
+// -------------------------- The start - when click crop update button --------------------------
+$("#crop-update").on('click', () => {
+
+    // get values from inputs
+    const cropCommonName = $("#cropCommonName").val();      // crop Common Name value
+    const cropScientificName = $("#cropScientificName").val();      // crop Scientific Name value
+    const fieldCode = $("#fieldNamesComboBox").val();        // field Code value
+    const cropCategory = $("#cropCategory").val();        // crop Category value
+    const cropSeason = $("#cropSeason").val();        // crop Season value
+    const cropImage = $("#cropImage")[0].files[0];       // file Name value
+
+    // check whether print those values
+    console.log("cropCommonName: " , cropCommonName);
+    console.log("cropScientificName: " , cropScientificName);
+    console.log("selectedFieldCode: " , fieldCode);
+    console.log("cropCategory: " , cropCategory);
+    console.log("cropSeason: " , cropSeason);
+
+
+    //let customerValidated = checkCustomerValidation(idOfCustomer,nameOfCustomer,addressOfCustomer,phoneOfCustomer);
+
+
+    //if(customerValidated) {
+
+    // Check for duplicate customer IDs
+    //isDuplicateCustomerId(idOfCustomer).then(isDuplicated => {
+
+    /*if (isDuplicated) {
+
+        // Show error message for duplicate customer ID
+        showErrorAlert("Customer ID already exists. Please enter a different ID.");
+
+    } else {*/
+
+    // Create a FormData object to send data as multipart/form-data
+    let formData = new FormData();
+    formData.append("cropCommonName", cropCommonName);
+    formData.append("cropScientificName", cropScientificName);
+    formData.append("cropCategory", cropCategory);
+    formData.append("cropSeason", cropSeason);
+    formData.append("fieldCode", fieldCode);
+
+    // Check if file is selected
+    if (cropImage) {
+        formData.append("cropImage", cropImage);  // Append the image file
+    }
+
+    // For testing
+    console.log("FormData Object : " + formData);
+
+
+    // ========= Ajax with JQuery =========
+
     $.ajax({
-        url: "http://localhost:5052/cropMonitoringSystem/api/v1/fields",
-        type: "GET",
+        url: "http://localhost:5052/cropMonitoringSystem/api/v1/crops",
+        type: "PUT",
+        data: formData,
+        processData: false, // Prevent jQuery from automatically transforming the data
+        contentType: false,
         headers: {
             "Authorization": "Bearer " + localStorage.getItem("token")
         },
-        success: function (response) {
 
-            // Assuming response is an array of FieldDto objects
-            const selectedFieldName = $("#fieldNamesComboBox");
-            selectedFieldName.empty(); // Clear existing options
-            selectedFieldName.append('<option value="">Choose a field</option>'); // Add default option
+        success: function (results) {
 
-            // Populate the select element with field names and IDs
-            response.forEach(field => {
-                const option = `<option value="${field.fieldCode}">${field.fieldName}</option>`;
-                selectedFieldName.append(option);
+            // show crop saved pop up
+            Swal.fire({
+                icon: 'success',
+                title: 'Crop updated successfully!',
+                showConfirmButton: false,
+                timer: 1500,
+                iconColor: 'rgba(131,193,170,0.79)'
             });
+
+            // load the table
+            //loadCustomerTable();
+
+            // clean the inputs values
+            $("#newCropModal form").trigger('reset');
+
+            // Remove the image preview
+            $("#previewImage").attr("src", "#").hide(); // Reset the image source and hide it
+            $("#noImageText").show();                  // Show the "No image selected" text
+
         },
+
         error: function (error) {
-            console.error("Error fetching fields:", error);
-            showErrorAlert("Failed to load fields. Please try again later.");
+            console.log(error)
+            showErrorAlert('Crop not updated...')
         }
     });
-}
-// -------------------------- The end - Function to fetch fields and populate the select element --------------------------
+    //}
+
+    //})
+
+    //}
+
+});
+// -------------------------- The end - when click crop update button --------------------------
 
 
 
